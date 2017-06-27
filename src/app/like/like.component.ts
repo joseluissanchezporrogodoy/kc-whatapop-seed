b31473672852aa51906c988b6bdfa7eb310c934f
JoseLuisSPG <joseluis.sanchezporro.godoy@gmail.com>
2017-06-27 18:29:14 +0200

    product id en hijo y funciones ok

--- src/app/like/like.component.ts
+++ src/app/like/like.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-like',
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./like.component.css']
 })
 export class LikeComponent implements OnInit {
-
+  @Input() productoID: number;
   constructor() { }
 
   ngOnInit() {
   }
+  onFavoriteClick(): void {
+    console.log(this.productoID);
+    //Guardar el producto
+    if (typeof(Storage) !== "undefined") {
+      // Setter
+      //localStorage.setItem("pageSize", 10);
+    }
+  }
 
+  isFavorite(productId: number): boolean {
+    console.log("ha ejecutado");
+    return null;
+  }
 }
